5bc0d52dbc73f889d7f6fd10e51645f59d406c2a
Nikhil-dr <nikhilgaidhani@hotmail.com>
2023-08-27 14:42:12 -0500

    pandas datagframe added
    read build-your-bowl ingredients file from S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
+## Importing Libraries
 import streamlit
+import pandas as pd
+
+###############################################################
 streamlit.title("My Parent's New Healthy Diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 and Blueberry Oatmeal")
@@ -7,3 +11,7 @@ streamlit.text("🐔 Hard/Soft-Boiled Free Range Chicken Eggs")
 streamlit.text("🥑🍞 Avacado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+##create pd dataframe to read CSV file from that S3 bucket
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
