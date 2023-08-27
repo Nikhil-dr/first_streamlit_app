5e528b6d29026bd5e96741caae2982dab4e0353a
Nikhil-dr <nikhilgaidhani@hotmail.com>
2023-08-27 14:34:46 -0500

    Header and text added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("My parent's new healthy diner")
+streamlit.title("My Parent's New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+streamlit.text("Hard/Soft-Boiled Free Range Chicken Eggs")
