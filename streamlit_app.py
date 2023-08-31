323eb40984c9e34256c4fbd70ec2e4e033ab0839
Nikhil-dr <nikhilgaidhani@hotmail.com>
2023-08-31 10:02:19 -0500

    normalize json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,9 @@ streamlit.header('FruityVice Fruit Advice')
 #Display Fruityvice api response
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
+
+# show json data in tabular form
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
